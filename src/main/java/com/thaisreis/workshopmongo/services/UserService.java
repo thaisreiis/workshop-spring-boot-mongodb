ac8497d5f3be0c87f46676ad8c5c05c1b1c9dc6f
Thais Reis <tdrv05@outlook.com>
2022-11-25 15:43:01 -0300

    Obtendo um usuário por id

--- src/main/java/com/thaisreis/workshopmongo/services/UserService.java
+++ src/main/java/com/thaisreis/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.thaisreis.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thaisreis.workshopmongo.domain.User;
 import com.thaisreis.workshopmongo.repository.UserRepository;
+import com.thaisreis.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,9 @@ public class UserService {
 		return repo.findAll();
 	}
 	
-	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 	
 }
---
+++ src/main/java/com/thaisreis/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.thaisreis.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+	
+	
+}
