467d8cab01bd89f85c5dfa995c246ca6da459690
Thais Reis <tdrv05@outlook.com>
2022-11-25 12:17:54 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/thaisreis/workshopmongo/config/Instantiation.java
@@ -0,0 +1,37 @@
+package com.thaisreis.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.thaisreis.workshopmongo.domain.User;
+import com.thaisreis.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+	
+	
+	
+	
+	
+}
