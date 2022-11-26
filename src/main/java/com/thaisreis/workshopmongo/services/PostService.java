ed346fff5afd2b7e0c938d09964349abfd257d35
Thais Reis <tdrv05@outlook.com>
2022-11-26 13:03:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/thaisreis/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.thaisreis.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thaisreis.workshopmongo.domain.Post;
+import com.thaisreis.workshopmongo.repository.PostRepository;
+import com.thaisreis.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+}
