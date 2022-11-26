f3bca549df13f01e2a1025ae2c86ffa7ad47de44
Thais Reis <tdrv05@outlook.com>
2022-11-26 14:08:50 -0300

    Consulta simples com query methods

--- src/main/java/com/thaisreis/workshopmongo/services/PostService.java
+++ src/main/java/com/thaisreis/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.thaisreis.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,10 @@ public class PostService {
 	    Optional<Post> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
 	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+	
 	
 }
